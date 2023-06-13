861f1906090bc9c26a997aa18d8362d9e298a0a3
sbstn-jmnz <sbstn.jmnz@yahoo.com>
2023-06-12 23:46:37 -0400

    solved good

--- src/dice_game.py
+++ src/dice_game.py
@@ -1,8 +1,9 @@
 import random
 option = 0
-wins = 0
-loose = 0
-even = 0
+total_user = 0
+total_computer = 0
+plays = 0
+
 while option != "3":
   print("----- El juego de los dados -----")
   print("1. Quiero Jugar")
@@ -17,31 +18,32 @@ while option != "3":
     user_dice_two = random.randint(1,6)
     # Suma de la partida del usuario
     user_sum = user_dice_one + user_dice_two
+    total_user += user_sum 
 
     # Dados del computador
     computer_dice_one = random.randint(1,6)
     computer_dice_two = random.randint(1,6)
     # Suma de la partida del computador
     computer_sum = computer_dice_one + computer_dice_two
+    total_computer += computer_sum
 
     print(f"Tus dados fueron: {user_dice_one} y {user_dice_two}")
     print(f"Los dados del computador fueron: {computer_dice_one} y {computer_dice_two}")
 
+    plays += 1
+
     # Gana el usuario Si la suma del usuario es mayor que el computador
     if user_sum > computer_sum:
-      wins += 1 
       print("Ganaste")
     elif user_sum < computer_sum:
-      loose += 1
       print("Perdiste")
     # Si son iguales empatan
     else:
-      even += 1
       print("Empate")
   elif option == "2":
-    print(f"Cantidad de partidas jugadas: {wins + loose + even}")
-    print(f"Puntaje total usuario: {wins}")
-    print(f"Puntaje total computadora: {loose}")
+    print(f"Cantidad de partidas jugadas: {plays}")
+    print(f"Puntaje total usuario: {total_user}")
+    print(f"Puntaje total computadora: {total_computer}")
   
   elif option =="3":
     print("Gracias por jugar.")
